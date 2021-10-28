1cba5cf82c9cb3bbd5c53805fa13270d9c2098ec
EduardoLoran <eduardotormes@hotmail.com>
2021-10-27 21:23:02 -0300

    Add front-end

---
+++ frontend/saudeApp/src/app/cadastro-plano-de-saude/cadastro-plano-de-saude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-plano-de-saude',
+  templateUrl: './cadastro-plano-de-saude.component.html',
+  styleUrls: ['./cadastro-plano-de-saude.component.css']
+})
+export class CadastroPlanoDeSaudeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/saudeApp/src/app/cadastro-unidade-saude/cadastro-unidade-saude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-unidade-saude',
+  templateUrl: './cadastro-unidade-saude.component.html',
+  styleUrls: ['./cadastro-unidade-saude.component.css']
+})
+export class CadastroUnidadeSaudeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
